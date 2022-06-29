23ed1b9feb669eeee0db42c6bf6a77f0e6f877be
Aashish Tiwari <aashish200198@gmail.com>
2022-06-29 19:35:23 +0530

    Updates

---
+++ precache-manifest.c57ac41614e6556e2e4899c7829240c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a5d5022e5344511f0a89",
+    "url": "/TextUtils/static/css/main.e4f60dee.chunk.css"
+  },
+  {
+    "revision": "a5d5022e5344511f0a89",
+    "url": "/TextUtils/static/js/main.a5d5022e.chunk.js"
+  },
+  {
+    "revision": "03ae7ea834bbe7645a20",
+    "url": "/TextUtils/static/js/1.03ae7ea8.chunk.js"
+  },
+  {
+    "revision": "946d8365c0317c9ba6aa",
+    "url": "/TextUtils/static/js/2.946d8365.chunk.js"
+  },
+  {
+    "revision": "14506651abc0b1064f2d",
+    "url": "/TextUtils/static/js/runtime~main.14506651.js"
+  },
+  {
+    "revision": "da67ce0c64646abc6c9306e6e039ba5e",
+    "url": "/TextUtils/index.html"
+  }
+];
